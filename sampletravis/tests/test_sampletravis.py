9eb31b94602e203ad096f0e8101533ade408e64b
dwanderson <anderson.dan.w@gmail.com>
2016-03-13 13:05:34 -0400

    Test works locally

--- sampletravis/tests/test_sampletravis.py
+++ sampletravis/tests/test_sampletravis.py
@@ -1,9 +1,12 @@
 import unittest
-import sampletravis
+
+#import foo
+#import sampletravis
+from sampletravis import foo
 
 class TestSampleTravis(unittest.TestCase):
     def test_double_int(self):
         num = 4
         expected = 8
-        self.assertEqual(expected, sampletravis.double(num))
+        self.assertEqual(expected, foo.double(num))
 
